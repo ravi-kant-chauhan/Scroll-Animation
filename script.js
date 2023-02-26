c60b64fa9faa3f8e1ad917f00a2c3d179727d363
Ravi <ravkchauhan1aip@gmail.com>
2023-02-26 23:41:55 +0530

    update

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ window.addEventListener('scroll', checkBoxes)
 checkBoxes()
 
 function checkBoxes() {
-    const triggerBottom = window.innerHeight / 5 * 4
+    const triggerBottom = window.innerHeight / 5 
 
     boxes.forEach(box => {
         const boxTop = box.getBoundingClientRect().top
