a935f3edad785e98817cf6c7fbd92b81d5f191de
Ravi <ravkchauhan1aip@gmail.com>
2023-02-26 23:41:35 +0530

    update

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ function checkBoxes() {
     boxes.forEach(box => {
         const boxTop = box.getBoundingClientRect().top
 
-        
+        if(boxTop < triggerBottom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+        }
     })
 }
